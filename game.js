fb0f1a80980b5424bcf5588d0c8f163959b7dcd2
ppayne12 <ppayne12@gmail.com>
2019-12-09 11:12:09 -0800

    updated with UI

--- game.js
+++ game.js
@@ -1,50 +1,64 @@
 'use strict';
 
-game(5);
 
-function game(numRounds){
-    let playerScore = 0;
-    let computerScore = 0;
-    for(let i = 0; i < numRounds; i++){
-        let roundResult;
-        let computerSelection = computerPlay();
-        let playerSelection = prompt("What do you want to play (rock, paper or scissor)?")
-        if(playerSelection){
-            
-            roundResult = playRound(playerSelection, computerSelection);
-            switch(roundResult){
-                case 0:
-                    console.log("It's a tie!");
-                    break;
-                case 1:
-                    console.log("You win! " + playerSelection + " beats " + computerSelection + ".");
-                    playerScore++;
-                    break;
-                case 2:
-                    console.log("You loose! " + computerSelection + " beats " + playerSelection + ".");
-                    computerScore++;
-                case 3:
-                    console.log("Unknown input - round skipped!");
-                    break;
+let playerScore = 0;
+let computerScore = 0;
 
-            }
-        }else{
-            console.log("Game Ended Prematurely!")
-            break;
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', game)
+});
+
+const hands = document.querySelectorAll('.hands');
+hands.forEach(hand => {
+    hand.addEventListener('transitionend', transition)
+});
+
+
+function game(e) {
+    this.firstElementChild.classList.add('playing');
+    const playerSelection = this.id;
+    const computerSelection = computerPlay();
+    const roundResult = playRound(playerSelection, computerSelection);
+
+    let resultDiv = document.querySelector('.results');
+    resultDiv.textContent = "";
+    resultDiv.insertAdjacentHTML('beforeend', '<p>and the winner is ...</p>');
+    setTimeout(() => {
+        switch (roundResult) {
+            case 0:
+                resultDiv.insertAdjacentHTML('beforeend', "<p>It's a tie!</p>");
+                break;
+            case 1:
+                resultDiv.insertAdjacentHTML('beforeend', `<p>You win! ${playerSelection} beats ${computerSelection}.</p>`);
+                playerScore++;
+                (document.querySelector('.playerScore')).textContent = String(playerScore);
+                break;
+            case 2:
+                resultDiv.insertAdjacentHTML('beforeend', `<p>You loose! ${computerSelection} beats ${playerSelection}.</p>`);
+                computerScore++;
+                (document.querySelector('.computerScore')).textContent = String(computerScore);
+                break;
+            case 3:
+                resultDiv.insertAdjacentHTML('beforeend', "<p>Unknown input - round skipped!</p>");
+                break;
+
         }
     }
+        , 1000);
 
-    if(playerScore > computerScore){
-        console.log("Game over. You win! " + playerScore + " to " + computerScore);
-    }else if(playerScore < computerScore){
-        console.log("Game over. You lose! " + playerScore + " to " + computerScore);
-    } else {
-        console.log("Game over. Tie! " + playerScore + " to " + computerScore);
+    if (computerScore > 4 || playerScore > 4) {
+        let instructionsBox = document.querySelector('.instructions');
+        instructionsBox.textContent = `Game Over!`
+
+
     }
+
 }
-function computerPlay(){
-    let rand = getRndInteger(0,2);
-    switch (rand){
+
+function computerPlay() {
+    let rand = getRndInteger(0, 2);
+    switch (rand) {
         case 0:
             return "Rock";
         case 1:
@@ -55,41 +69,47 @@ function computerPlay(){
 
 }
 
-//function returns 0 for tie, 1 for player win, 2 for computer win or 3 for invalid player input
-function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
+//function returns 0 for tie, 1 for player1 win, 2 for player2 win or 3 for invalid player(s) input
+function playRound(player1, player2) {
+    player1 = player1.toLowerCase();
+    player2 = player2.toLowerCase();
 
-    switch(playerSelection){
+    switch (player1) {
         case "rock":
-            if(computerSelection === "paper"){
-                return 2
-            }else if(computerSelection === "rock"){
+            if (player2 === "paper") {
+                return 2;
+            } else if (player2 === "rock") {
                 return 0;
-            }else{
+            } else if (player2 === "scissor") {
                 return 1;
             }
         case "paper":
-                if(computerSelection === "paper"){
-                    return 0;
-                }else if(computerSelection === "rock"){
-                    return 1;
-                }else{
-                    return 2;
-                }
+            if (player2 === "paper") {
+                return 0;
+            } else if (player2 === "rock") {
+                return 1;
+            } else if (player2 === "scissor") {
+                return 2;
+            }
         case "scissor":
-                if(computerSelection === "paper"){
-                    return 1;
-                }else if(computerSelection === "rock"){
-                    return 2;
-                }else{
-                    return 0;
-                }
+            if (player2 === "paper") {
+                return 1;
+            } else if (player2 === "rock") {
+                return 2;
+            } else if (player2 === "scissor") {
+                return 0;
+            }
         default:
             return 3;
     }
 }
 
+function transition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
+
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
